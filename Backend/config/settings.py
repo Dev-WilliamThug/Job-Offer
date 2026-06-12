e00da4b4a2e1cf4b3ad9e748bf180e40fff4dee0
Dev-WilliamThug <williamngassam2910@gmail.com>
2026-06-12 11:20:22 +0100

    fix static files

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,6 +96,14 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
